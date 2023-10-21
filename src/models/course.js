ea72ad5dfcbe60b99328ed010aba9f447f626704
Mohammed Badr <badrm0279@gmail.com>
2023-10-21 11:48:00 +0300

    added courses model

---
+++ src/models/course.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const CoursesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, "Please add course title"],
+        minlength: 2,
+        maxlength: 120,
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, "Please add course description"],
+        maxlength: 500,
+    },
+    weeks: { type: Number, required: [true, "Please add course duration"] },
+    tuition: { type: Number, required: [true, "Please add course fees"] },
+    minimumSkill: {
+        type: String,
+        enum: ["beginner", "intermediate", "advanced", "all"],
+        default: all,
+    },
+    scholarshipsAvailable: {
+        type: Boolean,
+        default: false,
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Bootcamp",
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+    },
+});
+
+module.exports = mongoose.model("Course", CoursesSchema);
