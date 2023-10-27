4009e0e7c97645ef64b777ccebc8a8844f42c84a
Mohammed Badr <badrm0279@gmail.com>
2023-10-27 08:12:41 +0200

    updated bootcamp router

--- src/router/bootcamp/bootcamp.router.js
+++ src/router/bootcamp/bootcamp.router.js
@@ -9,10 +9,12 @@ const {
 
 const router = Router();
 
+// router.use("/:bootcampId/courses")
+
 router.get("/", httpGetBootcamps);
 router.get("/:id", httpGetBootcamp);
 router.post("/", httpCreateBootcamp);
 router.put("/:id", httpUpdateBootcamp);
-router.delete("/:id", httpUpdateBootcamp);
+router.delete("/:id", httpDeleteBootcamp);
 
 module.exports = router;
