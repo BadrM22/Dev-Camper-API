fb727bbd4591bc721a3158ca0a2379ab6d832be3
Mohammed Badr <badrm0279@gmail.com>
2023-10-27 08:12:24 +0200

    added error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const apiV1 = require("./router/api-v1");
-
+const errorHandler = require("./middleware/errorhandler");
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -12,5 +12,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 app.use("/api/v1", apiV1);
+app.use(errorHandler)
 
 module.exports = app;
