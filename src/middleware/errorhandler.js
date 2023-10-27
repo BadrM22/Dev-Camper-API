89d4077fb54e3ebc38020594c799806206ee25d8
Mohammed Badr <badrm0279@gmail.com>
2023-10-27 09:14:09 +0200

    update

--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -4,7 +4,12 @@ const errorHandler = (error, req, res, next) => {
     let err = { ...error };
     err.message = error.message;
     console.log(`${error}`.red);
-
+    // if (error.name === "ValidationError") {
+    // }
+    // if (error.code === 11000) {
+    // }
+    // if (error.name === "CastError") {
+    // }
     return res.status(err.statusCode || 500).json({
         success: false,
         error: err.message || "Internal server error",
