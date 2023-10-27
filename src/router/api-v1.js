e195ffacb2dc41b14f6859444d2056acf7539414
Mohammed Badr <badrm0279@gmail.com>
2023-10-27 07:51:54 +0200

    added bootcamp router to api-v1

--- src/router/api-v1.js
+++ src/router/api-v1.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const AuthRouter = require("./auth/auth.router");
+const BootcampRouter = require("./bootcamp/bootcamp.router");
 
 const router = Router();
-router.use("/auth", AuthRouter);
 
+router.use("/auth", AuthRouter);
+router.use("/bootcamps", BootcampRouter);
 module.exports = router;
